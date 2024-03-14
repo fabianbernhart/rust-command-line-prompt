ad74f28350f3406731e745b49d9e0d5193bfe7ed
farbian_2005 <fabian_bernhart@web.de>
2024-03-14 23:50:07 +0100

    Get Started with creating Rust "echo" command promt

--- README.md
+++ README.md
@@ -3,8 +3,12 @@
 
 ## Task: 1 Rewriting existing tools in Rust can provide a great baseline to get started:
 
-- [ ] echo: repeats input
+- [ ] echo: repeats input (I am currently working on this!)
 - [ ] cat: concatenates files
 - [ ] ls: lists directoriesf
 - [ ] ind: locates files or directories
 - [ ] grep: matches text in files
+
+## Source
+
+- https://zerotomastery.io/blog/rust-practice-projects/
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::env::{args, Args};
 fn main() {
-    println!("Hello, world!");
+    let mut args: Args = args();
+    match args.len()  {
+        1 => {
+            println!("No arguments provided. try --help for help");
+            return;
+        },
+        _ => {},
+    }
+
+    let first_arg: String = args.nth(1).unwrap();
+    let skipped_ags = args.skip(0);
+    let mut result: String = "".to_string();
+    match first_arg.as_str()  {
+        "echo" => {
+            result= exec_echo(skipped_ags);
+        },
+        _ => println!("invalid string"),
+    }
+
+    print!("Console: {}", result)
+}
+
+#[test]
+fn test_exec_echo() {
+    let vec: Vec<String> = vec!["Hello".to_owned(), "World!".to_owned()];
+
+    let mut iter: std::slice::Iter<'_, String> = vec.iter();
+    assert_eq!("Hello World!", exec_echo(iter))
+}
+
+
+fn exec_echo(skipped_args: impl Iterator <Item = String>) -> String {
+
+    let mut echo_string: String = "".to_owned();
+
+
+    for arg in skipped_args  {
+
+        echo_string.push_str(&arg.to_owned())
+        
+    }
+
+    return echo_string;
+
 }
