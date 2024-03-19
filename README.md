12ecb4318621f2917cd8675c0d877868f1c19caa
Fabian Bernhart <135550293+fabianbernhart@users.noreply.github.com>
2024-03-19 19:31:36 +0100

    Get Started with creating Rust "echo" command promt (#1)
    * Get Started with creating Rust "echo" command promt
    
    - [x] add echo function 
    - [x] add unittests for echo function
    - [x]  Github Action Integration (Test workflow)

--- README.md
+++ README.md
@@ -7,8 +7,12 @@ Welcome to "Rust Command Line Prompt" where I @fabianbernhart try to to my best 
 
 ## Task: 1 Rewriting existing tools in Rust can provide a great baseline to get started:
 
-- [ ] echo: repeats input
+- [x] echo: repeats input
 - [ ] cat: concatenates files
 - [ ] ls: lists directoriesf
 - [ ] ind: locates files or directories
 - [ ] grep: matches text in files
+
+## Source
+
+- https://zerotomastery.io/blog/rust-practice-projects/
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+    let vec_args: Vec<String> = args().collect();
+
+    match vec_args.len() {
+        1 => {
+            help(vec_args);
+            return;
+        },
+        _ => {}
+    }
+
+
+    println!("{}", vec_args.join(" "));
+    
+
+    if vec_args[0] == "echo" {
+        let result: String = exec_echo(vec_args);
+        println!("{}", result)
+    }
+}
+
+fn help(args: Vec<String>) {
+
+    if args[0] == "echo" {
+        println!("NAME");
+        println!("%5s\nECHO: echo [...]");
+        
+        println!("%5s\n EXAMPLE: echo [...]");
+    }
+}
+
+
+#[test]
+fn test_exec_echo() {
+    let args: Vec<String> = vec!["echo".to_string(), "Hello".to_string(), "World!".to_string(), 911.to_string(), "💖".to_string()];
+    assert_eq!("Hello World! 911 💖", exec_echo(args));
+}
+
+
+fn exec_echo(mut args: Vec<String>) -> String {
+    args.remove(0);
+
+    let echo_string = args.join(" ");
+    return echo_string.to_string();
 }
