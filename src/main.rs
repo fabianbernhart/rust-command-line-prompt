45b95c52a5c42bf9852a6c9b6a4414ddc0576cfd
Fabian Bernhart <135550293+fabianbernhart@users.noreply.github.com>
2024-05-07 23:31:12 +0200

    ls: lists directories for rust command prompt (#8)

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,95 @@
 
-
+use std::ffi::OsString;
+use std::fmt::Error;
 use std::io::{self, Read, Write};
-use std::path::Path;
-use std::fs::File;
+use std::path::{Path, PathBuf};
+use std::fs::{self, File, ReadDir};
 
 use colored::Colorize;
 
-fn main() {
-    println!();
-    println!();
-    println!("Welcome to my Rust Command Line Prompt!");
 
+struct Command {
+    name: String,
+    args: Vec<String>
+}
+
+//todo: Refactor to parser.rs
+fn parse_command_input (input: &str) -> Command {
+    let mut parts = input.trim().split_whitespace();
+
+    let name = match parts.next() {
+        Some(first_part) => first_part.to_string(),
+        None => panic!("No name found in input"),
+    };
+
+    let args: Vec<String> = parts.map(|s| s.to_string()).collect();
+
+    Command {name, args}
+}
+
+
+pub enum ExecutionResult {
+    Success,
+    Failure,
+}
+
+
+// todo: Refactor to executor.rs
+fn execute_command(command: &Command) -> ExecutionResult {
+
+    let args: Vec<String> = command.args.clone();   
+
+    match command.name.as_str() {
+        "echo" => {
+            let _ = exec_echo(args);
+            ExecutionResult::Success
+        },
+        "ls" => {
+            ls();
+            ExecutionResult::Success
+        },
+        "cat" => {
+            let _ = cat_command(args);
+            ExecutionResult::Success
+        },
+
+        _ => {
+            println!("Command not found: {}", command.name);
+            ExecutionResult::Failure
+        }
+    }
+
+
+
+
+
+
+}
+
+fn welcome() {
+    println!();
+    println!();
+    println!("Welcome to my Rust Command Line Prompt!"); 
+}
+fn short_help() {
     println!("{}", "Commands:".yellow());
     println!("echo: echo [...]");
     println!("cat: cat [filename]");
     println!("exit: exit the prompt");
+    println!("ls: lists directory")
 
-    'command_prompt: loop {
+}
+
+
+fn main() {
+
+    welcome();
+    short_help();
+
+    loop {
         
         let mut input: String = String::new();
-        let mut args: Vec<String> = Vec::new();
-        let mut error_string: String = String::new();
-        let mut result_string: String = String::new();
-        let mut command: &str = &String::new();
-
         input.clear();
-        error_string.clear();
-        result_string.clear();
-        args.clear();
-
-
-
-
         print!("{}", "$ rsp ".magenta());
 
 
@@ -41,47 +99,25 @@ fn main() {
             .read_line(&mut input)
             .expect("Cannot read user input");
 
-        let mut splitted_input: std::str::SplitWhitespace<'_> = input.trim().split_whitespace();
 
-        if let Some(cmd) = splitted_input.next() {
-            command = cmd;
-            args.extend(splitted_input.map(String::from));
+        let command: Command = parse_command_input(&input);
+        let result: ExecutionResult = execute_command(&command);
+
+        match result {
+            ExecutionResult::Success => println!("Command executed successfully"),
+            ExecutionResult::Failure => println!("Command execution failed"),
         }
 
 
-        match command {
-            "exit" => break 'command_prompt,
-            "echo" => {
-                result_string = exec_echo(args.clone());
-            },
-            "cat" => {
-                if let Some(filename) = args.get(0) {
-                    match cat_command(filename) {
-                        Ok(contents) => {
-                            result_string = contents;
-                        }
-                        Err(e) => error_string = e.to_string(),
-                    }
-                } else {
-                    error_string = "No filename provided".to_string();
-                }
-            }
-
-            _ => error_string = "Command not found type help".to_string(),
-        }
-        
-        if !error_string.is_empty() {
-            println!("{} {}", "Error:".red().bold(), error_string)
-        } else {
-            println!("{} {}", "Result: \n".green().bold(), result_string)
-        }
     }
-    println!("Closing Command Prompt!");
 }
 
 
-fn exec_echo(args: Vec<String>) -> String {
-    args.join(" ")
+fn exec_echo(args: Vec<String>) -> Result<String, Error> {
+    let echo: String = args.join(" ");
+
+    print!("{}", echo);
+    Ok(echo)
 }
 
 #[cfg(test)]
@@ -90,13 +126,24 @@ mod echo_tests {
     #[test]
     fn test_exec_echo() {
         let args: Vec<String> = vec!["Hello".to_string(), "World!".to_string(), 911.to_string(), "💖".to_string()];
-    
-        assert_eq!("Hello World! 911 💖", exec_echo(args));
+
+
+        match exec_echo(args) {
+            Ok(result) => assert_eq!("Hello World! 911 💖", result),
+            Err(err) => panic!("Error: {}", err),
+        }
     }
 }
 
-fn cat_command(file_path: &str) -> io::Result<String> {
-    let path: &Path = Path::new(file_path);
+fn cat_command(args: Vec<String>) -> io::Result<String> {
+
+    let os_string: OsString = match args.first() {
+        Some(first_arg) => OsString::from(first_arg),
+        None => OsString::new(), // or any default value you want to use
+    };
+
+
+    let path: &Path = Path::new(&os_string);
     let mut file: File = File::open(path)?;
 
     let mut contents: String = String::new();
@@ -108,21 +155,60 @@ fn cat_command(file_path: &str) -> io::Result<String> {
 #[cfg(test)]
 mod cat_tests {
     use super::*;
-
+    
     #[test]
     fn test_cat_existing_file() {
-        let file_path = "test_file.txt";
-        let contents = "Hello, world!";
-        std::fs::write(file_path, contents).expect("Failed to write test file");
-        let result = cat_command(file_path);
-        assert_eq!(result.unwrap(), contents);
-        std::fs::remove_file(file_path).expect("Failed to remove test file");
+        let file_path = "test.txt";
+        let contents = "This file exists for testing my rust cat command";
+        fs::write(file_path, contents).expect("Failed to write test file");
+        let result: Result<String, io::Error> = cat_command(vec![file_path.to_string()]);
+
+        match result {
+            Ok(result) => assert_eq!(result, contents),
+            Err(err) => panic!("Error: {}", err),
+        }
+
+
+        
+        fs::remove_file(file_path).expect("Failed to remove test file");
     }
 
     #[test]
     fn test_cat_nonexistent_file() {
-        let result = cat_command("nonexistent_file.txt");
+        let result: Result<String, io::Error> = cat_command(vec!["nonexistent_file.txt".to_string()]);
         assert!(result.is_err());
     }
 }
+// todo: Refactor to ./commands/ls.rs
+fn ls () {
+    let paths: ReadDir = fs::read_dir("./").unwrap();
 
+    for path in paths {
+
+        let unwrapped_path: PathBuf = path.unwrap().path();
+
+        let is_file: bool = unwrapped_path.is_file();
+        let is_dir: bool = unwrapped_path.is_dir();
+        let display_path: String = unwrapped_path.display().to_string();
+
+        if is_file == true {
+            print!( "{} ", display_path.bold().green() );
+        } 
+
+        if is_dir == true {
+            print!( "{} ", display_path.green());
+        }
+    }
+}
+
+#[cfg(test)]
+mod ls_tests {
+    use super::ls;
+
+    #[test]
+    fn test_can_ls() {
+        ls()
+    }
+
+    // todo: Add more tests with mocks
+}
